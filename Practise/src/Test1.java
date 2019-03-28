d6b09941cd70a0ac02745bf60584b8ffb75a5d08
Balagopal <Balagopal@192.168.1.6>
2019-03-28 09:04:57 +0530

    all

---
+++ Practise/src/Test1.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+facts();
+	}
+	
+	public static void facts(){
+		int a[]= {1,4,6,73,82};
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("The numbers are");
+			System.out.println(a[i]);
+		}
+		
+		
+		
+		
+	}
+	
+}
